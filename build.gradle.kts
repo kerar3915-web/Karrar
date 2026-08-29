24eb394b734364b42bd7c68267c8dac4b189adcd
kerar3915-web <kerar3915@gmail.com>
2026-08-29 04:25:11 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
